739d479b0dab77f4711f5bd06a47ee6a2bae24bb
sami jaghouar <sami.jaghouar@jina.ai>
2023-07-20 23:42:52 +0200

    feat: add intgreation tes

--- jerboa2/train.py
+++ jerboa2/train.py
@@ -1,4 +1,5 @@
 from jerboa2.model.model import StarCoderTest, StarCoderTiny
+from jerboa2.dataset import TinyStoriesDataset
 from typer import Typer
 
 app = Typer(pretty_exceptions_enable=False)
@@ -7,4 +8,5 @@ app = Typer(pretty_exceptions_enable=False)
 @app.command()
 def train(debug: bool = False):
     model = StarCoderTest() if debug else StarCoderTiny()
-    _tokenizer = model.get_tokenizers()
+    tokenizer = model.get_tokenizer()
+    _dataset = TinyStoriesDataset(tokenizer=tokenizer, debug=debug)
---
+++ tests/integration/test_train.py
@@ -0,0 +1,5 @@
+from jerboa2.train import train
+
+
+def test_train():
+    train(debug=True)
