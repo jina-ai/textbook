d6f4b0bb8fe71d13c80056a3ec7d6bffb8c48204
samsja <sami.jaghouar@hotmail.fr>
2023-07-26 16:21:40 +0200

    feat: wip add mass generator
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- tests/units/test_dataset_gen.py
+++ tests/units/test_dataset_gen.py
@@ -1,5 +1,11 @@
-from textbook.dataset_gen import OpenAIGenerator
+from textbook.dataset_gen import OpenAIGenerator, mass_generation
+
 import pytest
+from unittest.mock import Mock, patch
+
+
+def mock_openai(mocker):
+    mocker.patch("textbook.dataset_gen.OpenAIGenerator.generate", return_value="Hello world WORLDDDDDDDDDDD")
 
 @pytest.mark.openai
 def test_generation():
@@ -8,7 +14,18 @@ def test_generation():
     assert isinstance(gen, str)
 
 def test_generation_mock(mocker):
+    mock_openai(mocker)
     generator = OpenAIGenerator()
-    mocker.patch("textbook.dataset_gen.OpenAIGenerator.generate", return_value="Hello world WORLDDDDDDDDDDD")
     gen = generator.generate("Hello world")
     assert isinstance(gen, str)
+
+
+
+def test_mass_generation(mocker):
+    mock_openai(mocker)
+    generator = OpenAIGenerator()
+
+    prompts = ["Hello world", "Goodbye world"]
+    results = mass_generation(prompts, generator, n=2)
+
+    assert len(results) == 4
--- textbook/dataset_gen.py
+++ textbook/dataset_gen.py
@@ -1,6 +1,8 @@
+import re
+from typing import List, Tuple
+
 import openai
-
-
+from rich.progress import Progress
 class OpenAIGenerator():
 
     def __init__(self, model: str = "gpt-3.5-turbo"):
@@ -10,3 +12,17 @@ class OpenAIGenerator():
     def generate(self, prompt: str) -> str:
         chat_completion = openai.ChatCompletion.create(model=self.model, messages=[{"role": "user", "content": "Hello world"}])
         return chat_completion.choices[0].message.contentss
+    
+
+
+def mass_generation(prompts: List[str], generator: OpenAIGenerator, n: int = 1) -> List[Tuple[str, str]]:
+
+    return_results: List[Tuple[str, str]] = []
+    with Progress() as progress:
+        task = progress.add_task("[red]Generating...", total=len(prompts)*n)
+        for _ in range(n):
+            for prompt in prompts:
+                return_results.append( (prompt, generator.generate(prompt)))
+                progress.advance(task)
+
+    return return_results
