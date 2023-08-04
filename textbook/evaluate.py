897374b8054c12bad6526206825faacffa016a4a
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-08-04 13:55:24 +0200

    feat: evaluation module

--- textbook/evaluate.py
+++ textbook/evaluate.py
@@ -1,13 +1,25 @@
-from typing import Any, Dict, List
+import json
+import re
+import tempfile
+from typing import Any, Dict, List, Optional
 
 import torch
 from transformers import GenerationConfig, PreTrainedTokenizer
 from human_eval.data import write_jsonl, read_problems, HUMAN_EVAL
+from human_eval.evaluation import evaluate_functional_correctness
 
 if torch.cuda.is_available():
     device = "cuda"
 else:
     device = "cpu"
+
+def read_jsonl_file(file_path):
+    data = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            json_data = json.loads(line)
+            data.append(json_data)
+    return data
 
 def generate_one_completion(model, tokenizer, prompt):
     inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
@@ -21,7 +33,14 @@ def generate_one_completion(model, tokenizer, prompt):
 
     s = generation_output.sequences[0]
     output = tokenizer.decode(s, skip_special_tokens=True)
+    matches = list(re.finditer(r'\bdef\s+\w+\s*\(', output))
+    if len(matches) > 1:
+        output = output[:matches[1].span()[0]]
 
+    if not output.startswith(prompt):
+        print('output will not be cleaned properly:', output)
+    else:
+        output = output[len(prompt):]
     return output
 
 def evaluate(
@@ -29,17 +48,30 @@ def evaluate(
     tokenizer: PreTrainedTokenizer,
     prompt_template: str = "{prompt}",
     eval_file: str = HUMAN_EVAL,
-) -> List[Dict[str, Any]]:
+    eval_size: Optional[int] = None
+):
     model.eval()
-
     problems = read_problems(evalset_file=eval_file)
-    problems = dict(list(problems.items())[0])
+    eval_size = eval_size or len(list(problems.items()))
+    problems = dict(list(problems.items())[:eval_size])
 
-
+    # since k=1, no need for more samples
     num_samples_per_task = 1
     samples = [
         dict(task_id=task_id, completion=generate_one_completion(model, tokenizer, prompt_template.format(prompt=problems[task_id]["prompt"])))
         for task_id in problems
         for _ in range(num_samples_per_task)
     ]
-    write_jsonl("samples.jsonl", samples)
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_file:
+        write_jsonl(temp_file.name, samples)
+
+        accuracy_results = evaluate_functional_correctness(temp_file.name, k=[1], problem_file=eval_file, problems=problems)
+        sample_results = read_jsonl_file(f"{temp_file.name}_results.jsonl")
+
+    # merge results and problems
+    results = {
+        item['task_id']: {**item, **problems[item['task_id']]}
+        for item in sample_results
+    }
+
+    return accuracy_results, results
